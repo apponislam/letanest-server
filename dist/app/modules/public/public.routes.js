8ef548d57175cf8a7d9be8ff5a44a3104544071e
Md. Appon Islam <appon2003@gmail.com>
2025-10-04 22:29:03 +0600

    public terms and condition added

--- dist/app/modules/public/public.routes.js
+++ dist/app/modules/public/public.routes.js
@@ -10,14 +10,9 @@ const public_controllers_1 = require("./public.controllers");
 const router = express_1.default.Router();
 router.post("/", auth_1.default, public_controllers_1.termsController.createTermsController);
 router.get("/", public_controllers_1.termsController.getAllTermsController);
-// Get T&C by ID (public)
 router.get("/:id", public_controllers_1.termsController.getTermByIdController);
-// Update T&C (logged in)
 router.put("/:id", auth_1.default, public_controllers_1.termsController.updateTermController);
-// Delete T&C (logged in)
 router.delete("/:id", auth_1.default, public_controllers_1.termsController.deleteTermController);
-// Get T&C by creator type (admin or host)
 router.get("/creator/:creatorType", public_controllers_1.termsController.getTermsByCreatorTypeController);
-// Get property-specific T&C
 router.get("/property/:propertyId", public_controllers_1.termsController.getPropertyTermsController);
 exports.publicRoute = router;
