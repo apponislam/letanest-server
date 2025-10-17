794d9ee05a01967129a04791a0edce660bf85012
apponislam <11appon11@gmail.com>
2025-10-17 06:06:50 +0600

    dashboard items created

--- src/app/modules/payment/payment.routes.ts
+++ src/app/modules/payment/payment.routes.ts
@@ -4,6 +4,12 @@ import { paymentControllers } from "./payment.controllers";
 
 const router = Router();
 
+// ADMIN ROUTES
+router.get("/admin", auth, paymentControllers.getAllPayments);
+router.get("/admin/totals", auth, paymentControllers.getPaymentTotals);
+router.get("/admin/stats/statistics", auth, paymentControllers.getPaymentStats);
+
+// other routes
 router.post("/create", auth, paymentControllers.createPayment);
 router.post("/confirm", auth, paymentControllers.confirmPayment);
 router.get("/my-payments", auth, paymentControllers.getMyPayments);
