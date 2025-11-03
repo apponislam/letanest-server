10eaeafde3c478d1e4755f9ab226ea1b4959a8ce
Md. Appon Islam <appon2003@gmail.com>
2025-11-04 01:32:32 +0600

    properties added new things 2

--- src/app/modules/subscribed/subscribed.routes.ts
+++ src/app/modules/subscribed/subscribed.routes.ts
@@ -4,16 +4,13 @@ import { userSubscriptionController } from "./subscribed.controllers";
 
 const router = express.Router();
 
-// All routes require authentication
-router.use(auth);
-
 // User subscription management
-router.post("/", userSubscriptionController.createUserSubscription);
-router.get("/my-subscriptions", userSubscriptionController.getMySubscriptions);
-router.get("/active", userSubscriptionController.getMyActiveSubscription);
-router.get("/:id", userSubscriptionController.getUserSubscription);
-router.patch("/:id/cancel", userSubscriptionController.cancelSubscription);
-router.patch("/:id/status", userSubscriptionController.updateSubscriptionStatus);
+router.post("/", auth, userSubscriptionController.createUserSubscription);
+router.get("/my-subscriptions", auth, userSubscriptionController.getMySubscriptions);
+router.get("/active", auth, userSubscriptionController.getMyActiveSubscription);
+router.get("/:id", auth, userSubscriptionController.getUserSubscription);
+router.patch("/:id/cancel", auth, userSubscriptionController.cancelSubscription);
+router.patch("/:id/status", auth, userSubscriptionController.updateSubscriptionStatus);
 
 // Webhook (might not need auth depending on your setup)
 router.post("/webhook/stripe", userSubscriptionController.handleStripeWebhook);
